4df0c0e8bd03a4ea90d7a1cb87c66ec2b506c1da
Nikhil Bodicharla <nbodicha@gmail.com>
2020-09-14 18:59:43 -0500

    added functionality for testing all four letter possible combinations

--- newsapp/cracker.py
+++ newsapp/cracker.py
@@ -2,42 +2,65 @@ import sqlite3
 import os
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.backends import default_backend
+import sys
+import base64
+from itertools import product
+from string import ascii_lowercase
 def main():
-	db = sqlite3.connect('db.sqlite3')
-	cursor = db.cursor()
-	cursor.execute("SELECT * from auth_user")
-	passwordData = cursor.fetchall()
-	commonPasswords = ["superfaketesting123"]
+	numArguments = len(sys.argv)
+	print(numArguments)
+	if numArguments == 2:
+		print(sys.argv)
+		passwordData = sys.argv[1].split('$')
+		print(passwordData)
+		if int(passwordData[1]) != 1:
+			print("Cannot brute-force password in time.")
+		else:
+			decodedPassword = base64.b64decode(passwordData[3])
+			for iteration in range(1, 5):
+				for i in product(ascii_lowercase, repeat = iteration):
+					word = ''.join(i)
+					print(word)
+	
+			
+			# Need to check all possible combinations and check if any hash equals decodedPassword
 
-	for x in passwordData:
-		passwordData = x[1].split('$')
-		for password in commonPasswords:
-			hashedPass = passwordHash(password, passwordData[2], passwordData[1])
-			if hashedPass == passwordData[3]:
-				print(",")
+
+
+	else:
+		db = sqlite3.connect('db.sqlite3')
+		cursor = db.cursor()
+		cursor.execute("SELECT * from auth_user")
+		passwordData = cursor.fetchall()
+		commonPasswords = ["superfaketesting123"]
+
+		for x in commonPasswords:
+			for password in passwordData:
+				passwordData = password[1].split('$')
+				hashedPass = passwordHash(x, passwordData[2], int(passwordData[1]))
+				if hashedPass == base64.b64decode(passwordData[3]):
+					print("The password that has been cracked is " + x)
+
+
 		
 
 def passwordHash(password, passSalt, iters):
-	print(passSalt)
-	saltInBytes = bytes(passSalt)
+	saltInBytes = bytes(passSalt, 'ascii')
 	kdf = PBKDF2HMAC(
 		algorithm=hashes.SHA256(),
 		length=32,
 		salt=saltInBytes,
 		iterations=iters,
-		backend = None
 	)
-	key = kdf.derive(b"my great password")
+	key = kdf.derive(bytes(password, 'ascii'))
 	kdf = PBKDF2HMAC(
-    	algorithm=hashes.SHA256(),
-    	length=32,
-    	salt=saltInBytes,
-    	iterations=iters,
-    	backend = default_backend(),
- )
- 	kdf.verify(password, key)
- 	return key
+		algorithm=hashes.SHA256(),
+		length=32,
+		salt=saltInBytes,
+		iterations=iters,
+	)
+	kdf.verify(bytes(password, 'ascii'), key)
+	return key
 
 
 
