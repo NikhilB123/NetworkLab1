0bca9443c37babda277b612e809f6bc472b59a05
NikhilB123 <nbodicha@gmail.com>
2020-09-14 19:25:25 -0500

    cracker.py is done, just need to add common passwords

--- newsapp/cracker.py
+++ newsapp/cracker.py
@@ -7,12 +7,11 @@ import base64
 from itertools import product
 from string import ascii_lowercase
 def main():
+	
 	numArguments = len(sys.argv)
-	print(numArguments)
+	foundPassword = False
 	if numArguments == 2:
-		print(sys.argv)
 		passwordData = sys.argv[1].split('$')
-		print(passwordData)
 		if int(passwordData[1]) != 1:
 			print("Cannot brute-force password in time.")
 		else:
@@ -20,18 +19,19 @@ def main():
 			for iteration in range(1, 5):
 				for i in product(ascii_lowercase, repeat = iteration):
 					word = ''.join(i)
-					print(word)
-	
-			
-			# Need to check all possible combinations and check if any hash equals decodedPassword
-
-
+					hashedPass = passwordHash(word, 'CPEtVdgaumD9', 1)
+					if hashedPass == b'v\xef\x91\xfa\xcd\xcc\x99\x7f\x96xm\xd4\xfc\xcdM\x10x\xf79>\xfe\xfd\xcb\xf8X\xaeu\x87\xb4\xce\xd2-':
+						print('Password cracked: ' + word)
+						foundPassword = True
+			if not foundPassword:
+				print('Password not cracked.')
 
 	else:
 		db = sqlite3.connect('db.sqlite3')
 		cursor = db.cursor()
 		cursor.execute("SELECT * from auth_user")
 		passwordData = cursor.fetchall()
+		print(passwordData)
 		commonPasswords = ["superfaketesting123"]
 
 		for x in commonPasswords:
@@ -39,10 +39,7 @@ def main():
 				passwordData = password[1].split('$')
 				hashedPass = passwordHash(x, passwordData[2], int(passwordData[1]))
 				if hashedPass == base64.b64decode(passwordData[3]):
-					print("The password that has been cracked is " + x)
-
-
-		
+					print("The password that has been cracked is " + x)	
 
 def passwordHash(password, passSalt, iters):
 	saltInBytes = bytes(passSalt, 'ascii')
